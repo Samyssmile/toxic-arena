6188d99eae67a9d8c5843a803bf764afa18ff002
Dejan Guberinic <dejan.guberinic@aclue.de>
2021-10-17 20:27:05 +0200

    added player , movement and bomb drop

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import LevelManager from "./level/level-manager.controller.js";
 
-
 let app = new PIXI.Application({ width: 768, height: 768 });
 
 document.body.appendChild(app.view);
@@ -8,7 +7,50 @@ document.body.appendChild(app.view);
 let levelManager = new LevelManager();
 levelManager.loadLevel(app);
 
+let keys = {};
+let keysDiv;
 
+let player = PIXI.Sprite.from("../src/assets/sprites/toxic-player-green.jpg");
+app.stage.addChild(player);
+player.height = 64;
+player.width = 64;
 
+window.addEventListener("keydown", keysDown);
+window.addEventListener("keyup", keysUp);
 
+app.ticker.add(gameLoop);
+keysDiv = document.querySelector("#keys");
 
+function keysDown(e) {
+  keys[e.keyCode] = true;
+}
+
+function keysUp(e) {
+  keys[e.keyCode] = false;
+}
+
+function gameLoop() {
+  if (keys["87"] || keys["38"]) {
+    player.y -= 5;
+  }
+  if (keys["65"] || keys["37"]) {
+    player.x -= 5;
+  }
+  if (keys["83"] || keys["40"]) {
+    player.y += 5;
+  }
+  if (keys["68"] || keys["39"]) {
+    player.x += 5;
+  }
+  if (keys["32"]) {
+    dropToxic(player.x, player.y);
+  }
+}
+
+function dropToxic(poitionX, positionY) {
+  let toxicBarrel = PIXI.Sprite.from("../src/assets/sprites/toxic-barrel.png");
+  app.stage.addChild(toxicBarrel);
+  toxicBarrel.position.set(poitionX, positionY);
+  toxicBarrel.height = 64;
+  toxicBarrel.width = 64;
+}
--- src/level/level-manager.controller.js
+++ src/level/level-manager.controller.js
@@ -1,44 +1,48 @@
 export default class LevelManager {
-    BLOCK_WIDTH = 64;
-    PATH_WALL = '../src/assets/levels/wall.png';
-    WALL_SIGN = "X"
-    PLAYER_SIGN = "P"
+  BLOCK_WIDTH = 64;
+  PATH_WALL = "../src/assets/levels/wall.png";
+  WALL_SIGN = "X";
+  PLAYER_SIGN = "P";
 
-    firstLevel =
-        "XXXXXXXXXXXX," +
-        "XEEEEXXXXEEX," +
-        "XXXXEXXXEEEX," +
-        "XEEXEEEEEEEX," +
-        "XEEXEXXEXEXX," +
-        "XEEXEEEEEEEX," +
-        "XEEXEEEEEEEX," +
-        "XEEXEEEXXXEX," +
-        "XEEEEEEXEEEX," +
-        "XEEXEEEXEEEX," +
-        "XEEXEEEEEEEX," +
-        "XXXXXXXXXXXX,"
-    ;
+  firstLevel =
+    "XXXXXXXXXXXX," +
+    "XEEEEXXXXEEX," +
+    "XXXXEXXXEEEX," +
+    "XEEXEEEEEEEX," +
+    "XEEXEXXEXEXX," +
+    "XEEXEEEEEEEX," +
+    "XEEXEEEEEEEX," +
+    "XEEXEEEXXXEX," +
+    "XEEEEEEXEEEX," +
+    "XEEXEEEXEEEX," +
+    "XEEXEEEEEEEX," +
+    "XXXXXXXXXXXX,";
 
+  loadLevel(app) {
+    console.log("Loading level. " + this.firstLevel);
 
-    loadLevel(app) {
-        console.log("Loading level. " + this.firstLevel)
-
-        let levelColumn = this.firstLevel.split(",");
-        for (let i = 0; i < levelColumn.length; i++) {
-            let levelRow = levelColumn[i];
-            for (let y = 0; y < levelRow.length; y++) {
-                let wall = PIXI.Sprite.from(this.PATH_WALL);
-                if (levelRow.charAt(y) === this.WALL_SIGN) {
-                    wall.y = i * this.BLOCK_WIDTH;
-                    wall.x = y * this.BLOCK_WIDTH;
-                    app.stage.addChild(wall);
-                } else {
-                }
-
-            }
-
+    let levelColumn = this.firstLevel.split(",");
+    for (let i = 0; i < levelColumn.length; i++) {
+      let levelRow = levelColumn[i];
+      for (let y = 0; y < levelRow.length; y++) {
+        let wall = PIXI.Sprite.from(this.PATH_WALL);
+        if (levelRow.charAt(y) === this.WALL_SIGN) {
+          wall.y = i * this.BLOCK_WIDTH;
+          wall.x = y * this.BLOCK_WIDTH;
+          app.stage.addChild(wall);
         }
-        return this.firstLevel;
+        if (levelRow.charAt(y) === this.PLAYER_SIGN) {
+          PATH_WALL = "../src/assets/levels/wall.png";
+          let player = PIXI.Sprite.from("../src/assets/sprites/toxic-player-green.jpg");
+          app.stage.addChild(player);
+          player.height = 64;
+          player.width = 64;
+          player.y = i * this.BLOCK_WIDTH;
+          player.x = y * this.BLOCK_WIDTH;
+          /* player.position.set(i * this.BLOCK_WIDTH, y * this.BLOCK_WIDTH); */
+        }
+      }
     }
-
+    return this.firstLevel;
+  }
 }
