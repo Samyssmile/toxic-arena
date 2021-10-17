eed9138781a6c7c49bcd3b7e00fba8faa5ce1c32
Samuel Abramov <info@abramov-samuel.de>
2021-10-18 01:16:30 +0200

    collision detection

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 import LevelManager from "./level/level-manager.controller.js";
 
-let app = new PIXI.Application({ width: 768, height: 768 });
+let app = new PIXI.Application({ width: 768, height: 768 , backgroundColor: 123207});
 
 document.body.appendChild(app.view);
 
-let levelManager = new LevelManager();
-levelManager.loadLevel(app);
+let levelManager = new LevelManager(app);
+levelManager.loadLevel();
 
 let keys = {};
 let keysDiv;
-
-let player = PIXI.Sprite.from("../src/assets/sprites/toxic-player-green.png");
-app.stage.addChild(player);
-player.height = 64;
-player.width = 64;
 
 window.addEventListener("keydown", keysDown);
 window.addEventListener("keyup", keysUp);
@@ -30,27 +25,26 @@ function keysUp(e) {
 }
 
 function gameLoop() {
+  const stepInPixels = 2;
   if (keys["87"] || keys["38"]) {
-    player.y -= 5;
-  }
+    levelManager.moveBottom(stepInPixels);
+
+  }else
   if (keys["65"] || keys["37"]) {
-    player.x -= 5;
-  }
+    levelManager.moveLeft(stepInPixels);
+
+  }else
   if (keys["83"] || keys["40"]) {
-    player.y += 5;
-  }
+    levelManager.moveTop(stepInPixels);
+
+  }else
   if (keys["68"] || keys["39"]) {
-    player.x += 5;
-  }
+    levelManager.moveRight(stepInPixels);
+
+  }else
   if (keys["32"]) {
-    dropToxic(player.x, player.y);
+    levelManager.dropToxicBomb();
   }
 }
 
-function dropToxic(poitionX, positionY) {
-  let toxicBarrel = PIXI.Sprite.from("../src/assets/sprites/toxic-barrel.png");
-  app.stage.addChild(toxicBarrel);
-  toxicBarrel.position.set(poitionX, positionY);
-  toxicBarrel.height = 64;
-  toxicBarrel.width = 64;
-}
+
--- src/level/level-manager.controller.js
+++ src/level/level-manager.controller.js
@@ -1,48 +1,172 @@
 export default class LevelManager {
-  BLOCK_WIDTH = 64;
-  PATH_WALL = "../src/assets/levels/wall.png";
-  WALL_SIGN = "X";
-  PLAYER_SIGN = "P";
+    BLOCK_WIDTH = 64;
+    PATH_WALL = "../src/assets/levels/wall.png";
+    WALL_SIGN = "X";
+    PLAYER_SIGN = "P";
+    wallArray = [];
+    player;
 
-  firstLevel =
-    "XXXXXXXXXXXX," +
-    "XEEEEXXXXEEX," +
-    "XXXXEXXXEEEX," +
-    "XEEXEEEEEEEX," +
-    "XEEXEXXEXEXX," +
-    "XEEXEEEEEEEX," +
-    "XEEXEEEEEEEX," +
-    "XEEXEEEXXXEX," +
-    "XEEEEEEXEEEX," +
-    "XEEXEEEXEEEX," +
-    "XEEXEEEEEEEX," +
-    "XXXXXXXXXXXX,";
+    firstLevel =
+        "XXXXXXXXXXXX," +
+        "XPEEEXXXXEEX," +
+        "XXXXEXXXEEEX," +
+        "XEEXEEEEEEEX," +
+        "XEEXEXXEXEXX," +
+        "XEEXEEEEEEX," +
+        "XEEXEEEEEEEX," +
+        "XEEXEEEXXXEX," +
+        "XEEEEEEXEEEX," +
+        "XEEXEEEXEEEX," +
+        "XEEXEEEEEEEX," +
+        "XXXXXXXXXXXX,"
+    ;
 
-  loadLevel(app) {
-    console.log("Loading level. " + this.firstLevel);
-
-    let levelColumn = this.firstLevel.split(",");
-    for (let i = 0; i < levelColumn.length; i++) {
-      let levelRow = levelColumn[i];
-      for (let y = 0; y < levelRow.length; y++) {
-        let wall = PIXI.Sprite.from(this.PATH_WALL);
-        if (levelRow.charAt(y) === this.WALL_SIGN) {
-          wall.y = i * this.BLOCK_WIDTH;
-          wall.x = y * this.BLOCK_WIDTH;
-          app.stage.addChild(wall);
-        }
-        if (levelRow.charAt(y) === this.PLAYER_SIGN) {
-          PATH_WALL = "../src/assets/levels/wall.png";
-          let player = PIXI.Sprite.from("../src/assets/sprites/toxic-player-green.png");
-          app.stage.addChild(player);
-          player.height = 64;
-          player.width = 64;
-          player.y = i * this.BLOCK_WIDTH;
-          player.x = y * this.BLOCK_WIDTH;
-          /* player.position.set(i * this.BLOCK_WIDTH, y * this.BLOCK_WIDTH); */
-        }
-      }
+    constructor(app) {
+        this.app = app;
     }
-    return this.firstLevel;
-  }
+
+    loadLevel() {
+        console.log("Loading level. " + this.firstLevel);
+
+        let levelColumn = this.firstLevel.split(",");
+        for (let i = 0; i < levelColumn.length; i++) {
+            let levelRow = levelColumn[i];
+            for (let y = 0; y < levelRow.length; y++) {
+                let wall = PIXI.Sprite.from(this.PATH_WALL);
+
+                if (levelRow.charAt(y) === this.WALL_SIGN) {
+                    wall.y = i * this.BLOCK_WIDTH;
+                    wall.x = y * this.BLOCK_WIDTH;
+                    this.app.stage.addChild(wall);
+                    this.wallArray.push(wall);
+                }
+                if (levelRow.charAt(y) === this.PLAYER_SIGN) {
+                    this.PATH_WALL = "../src/assets/levels/wall.png";
+                    this.player = PIXI.Sprite.from("../src/assets/sprites/toxic-player-green.png");
+                    this.player.getBounds();
+                    console.log("Player loaded: " + this.player)
+                    this.app.stage.addChild(this.player);
+                    this.player.height = 56;
+                    this.player.width = 56;
+                    this.player.y = i * this.BLOCK_WIDTH;
+                    this.player.x = y * this.BLOCK_WIDTH;
+                }
+            }
+        }
+        return this.firstLevel;
+    }
+
+    moveBottom(pixelStep) {
+        this.player.y -= pixelStep;
+        let isCollides = this.isCollision(this.player);
+
+        if (!isCollides) {
+            console.log(isCollides)
+
+        } else {
+            this.player.y += pixelStep;
+        }
+    }
+
+
+    moveLeft(pixelStep) {
+        this.player.x -= pixelStep;
+        let isCollides = this.isCollision(this.player);
+
+        if (!isCollides) {
+            console.log(isCollides)
+
+        } else {
+            this.player.x += pixelStep;
+        }
+
+    }
+
+    moveTop(pixelStep) {
+        this.player.y += pixelStep;
+        let isCollides = this.isCollision(this.player);
+
+        if (!isCollides) {
+            console.log(isCollides)
+
+        } else {
+            this.player.y -= pixelStep;
+        }
+    }
+
+    moveRight(pixelStep) {
+        this.player.x += pixelStep;
+        let isCollides = this.isCollision(this.player);
+
+
+        if (!isCollides) {
+            console.log(isCollides)
+
+        } else {
+            this.player.x -= pixelStep;
+        }
+    }
+
+
+    isCollision(shadowCopyPlayer) {
+        let collides = false;
+        for (let i = 0; i < this.wallArray.length; i++) {
+            let wall = this.wallArray[i];
+            collides = this.intersects(shadowCopyPlayer, wall);
+
+            if (collides) {
+                break;
+            }
+        }
+        return collides;
+    }
+
+    collisionDetection(playerSprite, wallSprite) {
+        let playerBox = playerSprite.getBounds();
+        let wallBox = wallSprite.getBounds();
+
+        playerSprite.get
+
+        /*        console.log("playerBox: "+playerBox)
+                console.log("wallBox: "+wallBox)*/
+
+        return playerBox.x + playerBox.width > wallBox.x &&
+            playerBox.x < wallBox.x + wallBox.width &&
+            playerBox.y + playerBox.height > wallBox.y &&
+            playerBox.y < wallBox.y + wallBox.height;
+    }
+
+    intersects(circle, rect) {
+        console.log("intersects")
+        let circleDistanceX = Math.abs(circle.x - rect.x);
+        let circleDistanceY = Math.abs(circle.y - rect.y);
+
+        let radiusDivider = 2.3
+        if (circleDistanceX > (rect.width / radiusDivider + circle.width / radiusDivider)) {
+            return false;
+        }
+        if (circleDistanceY > (rect.height / radiusDivider + circle.width / radiusDivider)) {
+            return false;
+        }
+
+        if (circleDistanceX <= (rect.width / radiusDivider)) {
+            return true;
+        }
+        if (circleDistanceY <= (rect.height / radiusDivider)) {
+            return true;
+        }
+
+        let cornerDistance_sq = (circleDistanceX - rect.width / radiusDivider) ^ 2 +
+            (circleDistanceY - rect.height / radiusDivider) ^ 2;
+
+        return (cornerDistance_sq <= ((circle.width/radiusDivider) ^ 2));
+    }
+
+    dropToxicBomb() {
+        let toxicBarrel = PIXI.Sprite.from("../src/assets/sprites/toxic-barrel.png");
+        this.app.stage.addChild(toxicBarrel);
+        toxicBarrel.position.set(this.player.x, this.player.y);
+        toxicBarrel.height = 64;
+        toxicBarrel.width = 64;
+    }
 }
