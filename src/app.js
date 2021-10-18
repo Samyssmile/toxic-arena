7f1f371ce6634a321c7a543848e62f11e5012b32
Samuel Abramov <info@abramov-samuel.de>
2021-10-18 13:40:07 +0200

    Larger Level

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import LevelManager from "./level/level-manager.controller.js";
 
-let app = new PIXI.Application({ width: 768, height: 768, backgroundColor: 123207 });
+let app = new PIXI.Application({width: 1024, height: 1024, backgroundColor: 123207});
 
 let firstLevel =
-  "XXXXXXXXXXXX," +
-  "XPEEEXXXXEEX," +
-  "XXXXEXXXEEEX," +
-  "XEEXEEEEEXEX," +
-  "XEEXEXXEXEEX," +
-  "XEEXEEEEEEX," +
-  "XEEXEEEEEEEX," +
-  "XEEXEEEXXXEX," +
-  "XEEEEEEXEEEX," +
-  "XEEXEEEXEEXX," +
-  "XEEXEEEEEEEX," +
-  "XXXXXXXXXXXX,";
+    "XXXXXXXXXXXXXXXX," +
+    "XPEEEXXXXEEXEEEX," +
+    "XXXXEXXXEEEXEEEX," +
+    "XEEEEEEEEXEXEEEX," +
+    "XEEXEXXEXEEXEXEX," +
+    "XXXXEEEEEEXEEXEX," +
+    "XEEXEEEEEEEEEXEX," +
+    "XEEXEEEXXXEXEEEX," +
+    "XEEEEEEXEEEXEEXX," +
+    "XEEXEEXXEEXXEEEX," +
+    "XEEXEEEEEXXXEEEX," +
+    "XEEXEEEEEEEXEEEX," +
+    "XEEXEEEXXEEXEEXX," +
+    "XEEXEEEXEEEXEXXX," +
+    "XEEXEEEXEEEXEEXX," +
+    "XXXXXXXXXXXXXXXX,";
 
 document.body.appendChild(app.view);
 
@@ -29,25 +33,25 @@ window.addEventListener("keyup", keysUp);
 app.ticker.add(gameLoop);
 
 function keysDown(e) {
-  keys[e.keyCode] = true;
+    keys[e.keyCode] = true;
 }
 
 function keysUp(e) {
-  keys[e.keyCode] = false;
+    keys[e.keyCode] = false;
 }
 
 function gameLoop() {
-  const stepInPixels = 4;
-  if (keys["87"] || keys["38"]) {
-    levelManager.moveTop(stepInPixels);
-  } else if (keys["65"] || keys["37"]) {
-    levelManager.moveLeft(stepInPixels);
-  } else if (keys["83"] || keys["40"]) {
-    levelManager.moveBottom(stepInPixels);
-  } else if (keys["68"] || keys["39"]) {
-    levelManager.moveRight(stepInPixels);
-  }
-  if (keys["32"]) {
-    levelManager.dropToxicBomb();
-  }
+    const stepInPixels = 4;
+    if (keys["87"] || keys["38"]) {
+        levelManager.moveTop(stepInPixels);
+    } else if (keys["65"] || keys["37"]) {
+        levelManager.moveLeft(stepInPixels);
+    } else if (keys["83"] || keys["40"]) {
+        levelManager.moveBottom(stepInPixels);
+    } else if (keys["68"] || keys["39"]) {
+        levelManager.moveRight(stepInPixels);
+    }
+    if (keys["32"]) {
+        levelManager.dropToxicBomb();
+    }
 }
